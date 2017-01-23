643110d0335f17527e8b4f1a0ca1d37d72038406
siddakarayan <sujeethrheen@gmail.com>
2017-01-23 12:01:13 +0530

    yes

--- myidea/WebContent/views/App.js
+++ myidea/WebContent/views/App.js
@@ -1,19 +1,73 @@
+var app=angular.module('Sign',['ngRoute']); 
 
-var app=angular.module("Sign",[]);
-app.controller('ctrl',function($scope,$http,$location,$window,$log)
-{
+app.controller('ctrl',function($scope,$http,$route,$location,$window)
+		
+		{
 var url="http://"+$window.location.host+"/myloss";
+        
+         alert("welcome buddy");
 
-
-alert("ha ha")
-
-$scope.Sign=function()
-{
-	alert("Hy sujeeth")
-	$http.post(url+'/Sign/'+$scope.name+'/'+$scope.email+'/'+$scope.password+'/'+$scope.address+'/'+$scope.state+'/'+$scope.city+'/'+$scope.mobile+'/').success(function(data)
-			{
-		alert("oiee");
-		$log.log(data);
-			});
+	           $scope.Login=function()
+	{
+         alert("get ready");
+	           
+               $http.post(url+'/Login/'+$scope.email+'/'+$scope.password).success(function(data)
+	{
+	console.log("U R inside");
+       
+	     alert("U U OUT");
+	     
+	     
+	    
+	   {
+		   
+		   $window.location.href = "http://localhost:8090/myidea/Home.html";
+	   }
+       
+         console.log(data);
+	      	
+      }).error(function()
+	{
+			   $window.alert("wrong username and password");
+	});
 	}
+	
+	
+    
+	           
+	           $scope.Sign=function()
+	{
+     alert("before post");
+	            $http.post(url+'/Sign/'+$scope.name+'/'+$scope.email+'/'+$scope.password+'/'+$scope.address+'/'+$scope.state+'/'+$scope.city+'/'+$scope.mobile).success(function(data)
+	{
+     alert("after post");
+                $location.path('/Login');
+                $scope.Login=function()
+    {
+	            $log.log(data);
+    }			
 	})
+	}
+	});
+	  
+
+          app.config(function($routeProvider){
+		  $routeProvider.when("/Login",{templateUrl: "Login.html"})
+		  $routeProvider.when("/Sign",{templateUrl:"Sign.html"})
+		  $routeProvider.when("/Home",{templateUrl: "Home.html"});
+	});
+
+		
+	    
+	
+	
+	
+		
+
+	
+
+
+	
+	
+	
+		
